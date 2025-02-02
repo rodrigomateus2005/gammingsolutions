913369dff04a81a8d2483983f623e06ae2d6d6c4
Rodrigo Alves <rodrigomateus2005@hotmail.com>
2025-02-02 20:16:11 -0300

    add joypad and audio handler

--- gammingsolutions_app/lib/screens/viewer.dart
+++ gammingsolutions_app/lib/screens/viewer.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'dart:developer';
 import 'dart:io';
 
+import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rfb/flutter_rfb.dart';
 import 'package:gamepads/gamepads.dart';
@@ -31,6 +33,8 @@ class _ViewerPagePageState extends State<ViewerPage> {
   @override
   void initState() {
     Gamepads.eventsByGamepad(widget.gamepadId).listen((event) {
+      socketJoypad.writeln(
+          {'key': event.key, 'value': event.value, 'type': event.type.index});
       log(event.key);
       log(event.value.toString());
       log(event.type.name);
@@ -38,19 +42,16 @@ class _ViewerPagePageState extends State<ViewerPage> {
 
     Socket.connect(widget.hostName, 9001).then((Socket sock) {
       socketAudio = sock;
-      socketAudio.listen(audioDataHandler,
-          onError: audioErrorHandler,
-          onDone: audioDoneHandler,
-          cancelOnError: false);
+      audioDataHandler(ChunkedStreamReader(socketAudio));
     }).catchError((e) {
       log("Unable to connect: $e");
     });
 
     Socket.connect(widget.hostName, 9001).then((Socket sock) {
       socketJoypad = sock;
-      socketJoypad.listen(audioDataHandler,
-          onError: audioErrorHandler,
-          onDone: audioDoneHandler,
+      socketJoypad.listen(joypadDataHandler,
+          onError: joypadErrorHandler,
+          onDone: joypadDoneHandler,
           cancelOnError: false);
     }).catchError((e) {
       log("Unable to connect: $e");
@@ -58,7 +59,7 @@ class _ViewerPagePageState extends State<ViewerPage> {
 
     startAudioPlayer();
 
-    super.initState();    
+    super.initState();
   }
 
   Future<void> startAudioPlayer() async {
@@ -66,13 +67,16 @@ class _ViewerPagePageState extends State<ViewerPage> {
     audioStream.init(channels: 2, sampleRate: 48000);
   }
 
-  Future<void> audioDataHandler(data) async {
-    audioStream.push(data.buffer.asFloat32List());
+  Future<void> audioDataHandler(ChunkedStreamReader<int> reader) async {
+    while (true) {
+      var data = await reader.readBytes(1024 * 50);
+      audioStream.push(data.buffer.asFloat32List());
+    }
   }
 
-  void joypadDataHandler(data) {  
-    print(new String.fromCharCodes(data).trim());
-  } 
+  void joypadDataHandler(data) {
+    log(String.fromCharCodes(data).trim());
+  }
 
   void audioErrorHandler(error, StackTrace trace) {
     log(error);
