d44b951331a6259d5345d562558d78a0478fe781
Rodrigo Alves <rodrigo.alves@jnmoura.com.br>
2025-02-02 23:00:54 -0300

    improves

--- gammingsolutions_app/lib/screens/home.dart
+++ gammingsolutions_app/lib/screens/home.dart
@@ -5,15 +5,6 @@ import 'package:gammingsolutions_app/widgets/list_gamepads.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -21,11 +12,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   String gamepadIdSelected = '';
   final hostController = TextEditingController();
   final passwordController = TextEditingController();
 
   void _startViewer() {
+    _formKey.currentState?.save();
+
     Navigator.push(
       context,
       MaterialPageRoute(
@@ -55,57 +50,46 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              controller: hostController,
-              decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: 'Digite o host',
-              ),
-            ),
-            TextFormField(
-              controller: passwordController,
-              decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: 'Digite a senha',
-              ),
-              autocorrect: false,
-              enableSuggestions: false,
-              obscureText: true,
-            ),
-            ListGamepads(
-              onSelected: (value) {
-                setState(() {
-                  gamepadIdSelected = value;
-                });
-              },
-            ),
-          ],
-        ),
-      ),
+      body: Form(
+          key: _formKey,
+          child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextFormField(
+                      controller: hostController,
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Digite o host',
+                      ),
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Digite a senha',
+                      ),
+                      autocorrect: false,
+                      enableSuggestions: false,
+                      obscureText: true,
+                    ),
+                    ListGamepads(
+                      onSelected: (value) {
+                        setState(() {
+                          gamepadIdSelected = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ))),
       floatingActionButton: FloatingActionButton(
         onPressed: _startViewer,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        tooltip: 'Play',
+        child: const Icon(Icons.play_arrow),
+      ),
     );
   }
 }
